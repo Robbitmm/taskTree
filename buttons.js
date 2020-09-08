3525acf5bdd44fdd416a8e3ae48b69508cf5ba95
Robbitmm <46769452+Robbitmm@users.noreply.github.com>
2020-09-08 20:18:41 -0300

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,234 @@
+﻿function TreeMenu(){
+  let span = document.createElement('span');
+
+  for (let li of tree.querySelectorAll('li')){
+      li.prepend(span);
+      span.append(span.nextSibling); // move the text node into span
+  }
+
+    // catch clicks on whole tree
+  tree.onclick = function(event) {
+
+    if (event.target.tagName != 'SPAN') {
+      return;
+    }
+
+    let childrenContainer = event.target.parentNode.querySelector('ul');
+    if (!childrenContainer) return; // no children
+
+    childrenContainer.hidden = !childrenContainer.hidden;
+  }
+}
+
+function AddItem(){
+  target = event.target;
+  //console.log("step 1: "+target.value);
+
+  Ulparent = target.closest('ul');
+  Liparent = target.closest('li');
+  //console.log("step 2: Ok");
+
+  NewItem = document.createElement('li');//Create new Item
+  //NewItem.innerHTML = 'Item ' + Ulparent.childElementCount; //Nomeia o item de acordo com sua posição (número)
+
+  input = document.createElement('input');
+  NewItem.append(input);
+  span = document.createElement('span');
+  span.setAttribute('ondblclick', 'ChangeName()');
+
+  NewItem.prepend(span);
+  span.append(span.nextSibling);
+
+  Ulparent.insertBefore(NewItem, Liparent);
+  
+  input.focus();
+
+  input.onblur = function(){
+    //Change name
+    NewItem.setAttribute('id', input.value)
+    console.log(input.value);
+    span.innerHTML = input.value;
+
+    input.remove();
+    console.log("removed");
+
+    //Add button
+    Addbutton = document.createElement('button');
+    Addbutton.setAttribute('class', 'Btn ItemBtn');
+    Addbutton.onclick = function (){
+      AddSubLevel();
+    }
+  
+    Addbutton.innerHTML = "+";
+    NewItem.append(Addbutton);
+  
+    //Done Button
+    Donebutton = document.createElement('button');
+    Donebutton.setAttribute('class', 'Btn ItemBtn done');
+  
+    Donebutton.onclick = function (){
+      CheckTask();
+    };
+    Donebutton.innerHTML = "&check;"; //Display check symbol
+    NewItem.append(Donebutton);
+  
+    //Remove Button
+    Removebutton = document.createElement('button');
+    Removebutton.setAttribute('class', 'Btn ItemBtn remove');
+  
+    Removebutton.onclick = function (){
+      RemoveTask();
+    };
+    Removebutton.innerHTML = "x"; //Display check symbol
+  
+    NewItem.append(Removebutton); 
+
+    //Percentage
+    progress = document.createElement('label');
+    progress.setAttribute('class', 'Btn progress');
+
+    progress.innerHTML = '(0%)'
+    NewItem.append(progress);
+  }
+}
+
+function AddSubLevel(){
+  console.log('Add Second Level');
+  span = document.createElement('span');
+  span.setAttribute('ondblclick', 'ChangeName()');
+
+  UlChild = document.createElement('ul');
+  LiChild = document.createElement('li');
+  LiName = document.createElement('input');
+  LiName.setAttribute('type', 'text');
+
+  ParentContainer = event.target.closest('li');
+
+  ParentContainer.append(UlChild);
+  UlChild.append(LiChild);
+  LiChild.append(span);
+
+  LiChild.append(LiName);
+
+  LiName.focus();
+
+  LiName.onblur = function(){
+    LiChild.setAttribute('id', LiName.value)
+    console.log(LiName.value);
+    span.innerHTML = LiName.value;
+
+    LiName.remove();
+    console.log("removed");
+
+    //Add Button
+    Addbutton = document.createElement('button');
+    Addbutton.setAttribute('class', 'Btn ItemBtn');
+
+    Addbutton.onclick = function (){
+      AddSubLevel();
+    }
+    Addbutton.innerHTML = "+";
+    LiChild.append(Addbutton);
+
+    //Done button
+    Donebutton = document.createElement('button');
+    Donebutton.setAttribute('class', 'Btn ItemBtn done');
+
+    Donebutton.onclick = function (){
+      CheckTask();
+    };
+    Donebutton.innerHTML = "&check;";
+
+    LiChild.append(Donebutton);
+
+    //Remove button
+    Removebutton = document.createElement('button');
+    Removebutton.setAttribute('class', 'Btn ItemBtn remove');
+
+    Removebutton.onclick = function (){
+      RemoveTask();
+    };
+    Removebutton.innerHTML = "x";
+
+    LiChild.append(Removebutton);
+
+    //Percentage
+    progress = document.createElement('label');
+    progress.setAttribute('class', 'Btn progress');
+
+    progress.innerHTML = '(0%)'
+    LiChild.append(progress);
+  }
+}
+
+function ChangeName(){
+  event.target.contentEditable = true;
+
+  event.target.onblur = function(){
+    event.target.contentEditable = false;
+
+    H1 = document.getElementById("title").textContent;
+    document.title = H1;
+  }
+}
+
+function CheckTask(){
+  //console.log("check Test");
+
+  checked = event.target;
+  addbutton = checked.previousSibling;
+  span = addbutton.previousSibling;
+  close = checked.nextSibling;
+  progress = close.nextSibling;
+  progress.innerHTML = '(100%)';
+
+  if(checked.classList.contains('Done')){
+    //console.log("romoveded line");
+    span.removeAttribute('style');
+    checked.classList.remove('Done');
+    progress.textContent = Progress();
+    return;
+  }
+
+  span.setAttribute('style', 'text-decoration: line-through');
+  progress.textContent = Progress();
+
+  checked.classList.add('Done');
+}
+
+function RemoveTask(){
+  Li = event.target.closest('li');
+  Li.remove();
+}
+
+function Progress(){
+  LiGet = event.target.closest('li');
+  InsideUl = LiGet.getElementsByTagName('ul');
+  InsideUl.child
+  //checkedKids = InsideUl.getElementsByClassName('Done');
+
+  counter = InsideUl.length;
+  console.log(counter); 
+
+  if(counter == 0){
+    return '(0%)';
+  }else{
+    doneKids();
+    result = (100/counter);
+    //console.log(result);
+    return `(${result}%)`
+  }
+}
+
+function doneKids(){
+  if(LiGet.classList.contains('Done')){
+    LiGet.removeAttribute('style');
+    LiGet.classList.remove('Done');
+    return;
+  }
+
+  for (var i = 0; i < counter; i++) {
+    LiGet.setAttribute('style', 'text-decoration: line-through');
+    LiGet.classList.add('Done');
+  };
+}
